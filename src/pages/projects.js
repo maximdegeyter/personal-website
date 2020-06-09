d799e98aa83cdb9d67341afa98a986bde1dc2c11
maximdegeyter <maximdegeyter@outlook.com>
2020-06-09 23:59:18 +0200

    feat: create projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,14 +1,38 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import SubTitle from "../components/subtitle/subtitle"
 import SEO from "../components/seo"
+import LinkImg from "../images/visit.png"
+import SiteImg from "../images/personalWebsite.png"
+
+import "./projects.css"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Projects" />
-    <h1>Projects</h1>
-    <p className="subtitle">What I've been working on</p>
-    <p>Coming soon!</p>
+    <SubTitle title="Projects" />
+    <p>What I've been working on</p>
+    <ul>
+      <li>
+        <article className="project">
+          <a
+            className="project--link"
+            href="https://maximdegeyter.netlify.app/"
+          >
+            <h3>My personal website</h3>
+            <img className="project--link-img" src={LinkImg}></img>
+          </a>
+          <div className="img-hover-zoom">
+            <img src={SiteImg}></img>
+          </div>
+          <ul className="project--tagList">
+            <li className="project--tag">Gatsby</li>
+            <li className="project--tag">Netlify CMS</li>
+          </ul>
+        </article>
+      </li>
+    </ul>
   </Layout>
 )
 
