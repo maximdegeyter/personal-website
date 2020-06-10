c8ce92f7cf7ba6483fe3e7778e192b71d57685e2
maximdegeyter <maximdegeyter@outlook.com>
2020-06-10 02:53:19 +0200

    feat: add & use data from cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/projects`,
+        name: `projects`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SubTitle from "../components/subtitle/subtitle"
@@ -8,32 +9,57 @@ import SiteImg from "../images/personalWebsite.png"
 
 import "./projects.css"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Projects" />
-    <SubTitle title="Projects" />
-    <p>What I've been working on</p>
-    <ul>
-      <li>
-        <article className="project">
-          <a
-            className="project--link"
-            href="https://maximdegeyter.netlify.app/"
-          >
-            <h3>My personal website</h3>
-            <img className="project--link-img" src={LinkImg}></img>
-          </a>
-          <div className="img-hover-zoom">
-            <img src={SiteImg}></img>
-          </div>
-          <ul className="project--tagList">
-            <li className="project--tag">Gatsby</li>
-            <li className="project--tag">Netlify CMS</li>
-          </ul>
-        </article>
-      </li>
-    </ul>
-  </Layout>
-)
+const SecondPage = ({ data }) => {
+  const projects = data.allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Projects" />
+      <SubTitle title="Projects" />
+      <p>What I've been working on</p>
+      <ul>
+        {projects.map(project => (
+          <li>
+            <article className="project">
+              <a
+                className="project--link"
+                href={project.node.frontmatter.url}
+                target="_blank"
+              >
+                <h3>{project.node.frontmatter.title}</h3>
+                <img className="project--link-img" src={LinkImg}></img>
+              </a>
+              <div className="img-hover-zoom">
+                <img src={SiteImg}></img>
+              </div>
+              <ul className="project--tagList">
+                {project.node.frontmatter.tags.map(tag => (
+                  <li className="project--tag">{tag}</li>
+                ))}
+              </ul>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export default SecondPage
+
+export const pageQuery = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            image
+            tags
+            title
+            url
+          }
+        }
+      }
+    }
+  }
+`
