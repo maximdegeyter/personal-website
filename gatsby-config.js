4e5ad1eaea6bd882c281a10ef32cd9b2b761207a
maximdegeyter <maximdegeyter@outlook.com>
2020-06-13 02:29:10 +0200

    fix: hero text animation

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `Quicksand`,
+          `Quicksand\:300,400,700`,
           `Avenir Next`,
           `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
         ],
